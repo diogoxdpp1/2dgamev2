17fe5a54a2bd689cd6340e26dd25e3e487ae54ca
diogoxdpp1 <glhf077@gmail.com>
2022-02-08 15:56:31 +0000

    edit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,12 @@
 package com.company;
-
+import javax.swing.*;
 public class Main {
-
     public static void main(String[] args) {
-	// write your code here
-    }
+                JFrame frame = new JFrame("My First GUI");
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.setSize(300,300);
+                JButton button = new JButton("Click me");
+                frame.getContentPane().add(button); // Adds Button to content pane of frame
+                frame.setVisible(true);
+            }
 }
