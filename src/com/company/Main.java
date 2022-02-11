3991cfda6e5529852a56e66d88cb8712c35ed049
still-not-telling <JR212384@reigate.ac.uk>
2022-02-11 10:55:17 +0000

    Changes by dd211037

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,13 @@
 package com.company;
 import com.company.frontEnd.GUI;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
+import javax.swing.JPanel;
 
 import javax.swing.*;
 public class Main {
@@ -7,10 +15,10 @@ public class Main {
                 JFrame frame = new JFrame("My First GUI");
                 frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
                 GUI GUI = new GUI(300, 300);
+                BufferedImage myPicture = ImageIO.read(new File("path-to-file"));
+                JLabel picLabel = new JLabel(new ImageIcon(myPicture));
+                frame.setIconImage(picLabel);
                 frame.setSize(GUI.getGridHeight(), GUI.getGridLength());
                 frame.setVisible(true);
             }
-
-            aa
-
 }
